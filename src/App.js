e1d3aa181762ebd8e5a1a518fccc03b764b87a7f
Ritesh <riteshkashinathkalyanshetti03@gmail.com>
2023-10-25 20:30:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Board from './Component/Board';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Square.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { useState } from 'react';
+
+const Square = ({value,onSquareClick}) => {
+
+    
+
+    return (
+        <>
+            <button className = 'square' onClick={onSquareClick}> {value} </button>
+        </>
+    )
+}
+
+export default Square
